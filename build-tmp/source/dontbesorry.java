322a5c566b6acebdd867d9d82eb9adaa109e569e
chaseabm <chbush-mclaughlin@s.sfusd.edu>
2017-11-25 10:15:05 -0800

    basic functionality

--- build-tmp/source/dontbesorry.java
+++ build-tmp/source/dontbesorry.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class dontbesorry extends PApplet {
 
 ArrayList <String> displayedMessages = new ArrayList <String>();
-String inputString;
+String inputString = "";
 public void setup()
 {
 	
@@ -26,19 +26,41 @@ public void setup()
 public void draw()
 {
 	background(0);
+	fill(130);
+	rect(0, 0, width, 40);
+	fill(255);
 	textSize(20);
-	text("this is text", 20, 200);
+	text("DON'T BE SORRY", 350, 25);
+	text(inputString, 15, height-15);
+	stroke(255);
+	line(0, height-40, width, height-40);
+	int j = displayedMessages.size() - 1;
+	for (int i = height-60; i > 60; i -= 30)
+	{
+		if (displayedMessages.size() == 0 || j == -1)
+			break;
+		System.out.println("i is " + i);
+		System.out.println("j is " + j);
+		fill(255);
+		text(displayedMessages.get(j), 15, i);
+		j--;
+	}
 }
 public void keyPressed()
 {
 	if (key == DELETE || key == BACKSPACE)
-		inputString = inputString.substring(0, inputString.length());
+	{
+		inputString = inputString.substring(0, inputString.length()-1);
+	}
 	else if (key == ENTER || key == RETURN)
+	{
 		displayedMessages.add(inputString);
+		inputString = "";
+	}
 	else
 		inputString += key;
 }
-  public void settings() { 	size(700, 500); }
+  public void settings() { 	size(800, 500); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "dontbesorry" };
     if (passedArgs != null) {
--- dontbesorry.pde
+++ dontbesorry.pde
@@ -1,8 +1,8 @@
 ArrayList <String> displayedMessages = new ArrayList <String>();
-String inputString;
+String inputString = "";
 public void setup()
 {
-	size(700, 500);
+	size(800, 500);
 	PFont mono;
 	mono = loadFont("Consolas-20.vlw");
 	textFont(mono);
@@ -10,15 +10,37 @@ public void setup()
 public void draw()
 {
 	background(0);
+	fill(130);
+	rect(0, 0, width, 40);
+	fill(255);
 	textSize(20);
-	text("this is text", 20, 200);
+	text("DON'T BE SORRY", 350, 25);
+	text(inputString, 15, height-15);
+	stroke(255);
+	line(0, height-40, width, height-40);
+	int j = displayedMessages.size() - 1;
+	for (int i = height-60; i > 60; i -= 30)
+	{
+		if (displayedMessages.size() == 0 || j == -1)
+			break;
+		System.out.println("i is " + i);
+		System.out.println("j is " + j);
+		fill(255);
+		text(displayedMessages.get(j), 15, i);
+		j--;
+	}
 }
 public void keyPressed()
 {
 	if (key == DELETE || key == BACKSPACE)
-		inputString = inputString.substring(0, inputString.length());
+	{
+		inputString = inputString.substring(0, inputString.length()-1);
+	}
 	else if (key == ENTER || key == RETURN)
+	{
 		displayedMessages.add(inputString);
+		inputString = "";
+	}
 	else
 		inputString += key;
 }
