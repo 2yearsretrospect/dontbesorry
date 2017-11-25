440d11971da1462d17c8965b964e4d31e1e6e907
Sebastian Fischer <seabassfishes@gmail.com>
2017-11-25 11:49:23 -0800

    Quests & wrapping

--- build-tmp/source/dontbesorry.java
+++ build-tmp/source/dontbesorry.java
@@ -14,14 +14,29 @@ import java.io.IOException;
 
 public class dontbesorry extends PApplet {
 
+//import java.util.Map;
 ArrayList <String> displayedMessages = new ArrayList <String>();
 String inputString = "";
+StringDict[] quest1;
 public void setup()
 {
 	
 	PFont mono;
 	mono = loadFont("Consolas-20.vlw");
 	textFont(mono);
+	Cmd[] initialCommands_quest1 = {
+		new Cmd(
+			"walk in",
+			"You enter the elder's home. He asks you for a reminder of your name."),
+		new Cmd(
+			"walk away",
+			"You walk away and return to your father's farm where you work the land for the rest of your life, and even have a couple of kids. You die at the age of 34 from tuberculosis with many regrets.")
+	};
+	Quest quest1 = new Quest(
+		"Welcome to Don't Be Sorry! You are a member of the plebian masses of Landia, your hometown. Yesterday you turned 16, the age of coming in your rather primal society, and chose the vocation of Adventurer. You are currently standing outside your respective elder's abode, contemplating entrance and seriously doubting yesterday's decision.",
+		initialCommands_quest1
+		);
+	displayedMessages.add(quest1.initialMessage);
 }
 public void draw()
 {
@@ -34,13 +49,24 @@ public void draw()
 	text(inputString, 15, height-15);
 	stroke(255);
 	line(0, height-40, width, height-40);
+	if (displayedMessages.size() != 0 && displayedMessages.get(displayedMessages.size()-1).length() >= 71)
+	{
+		String left_fragment = "";
+		String right_fragment = displayedMessages.get(displayedMessages.size()-1);
+		displayedMessages.remove(displayedMessages.size()-1);
+		while(right_fragment.length() >= 71)
+		{
+			left_fragment = right_fragment.substring(0, 70);
+			right_fragment = right_fragment.substring(70);
+			displayedMessages.add(left_fragment);
+		}
+		displayedMessages.add(right_fragment);
+	}
 	int j = displayedMessages.size() - 1;
 	for (int i = height-60; i > 60; i -= 30)
 	{
 		if (displayedMessages.size() == 0 || j == -1)
 			break;
-		System.out.println("i is " + i);
-		System.out.println("j is " + j);
 		fill(255);
 		text(displayedMessages.get(j), 15, i);
 		j--;
@@ -48,7 +74,7 @@ public void draw()
 }
 public void keyPressed()
 {
-	if (key == DELETE || key == BACKSPACE)
+	if ((key == DELETE || key == BACKSPACE) && inputString.length() != 0)
 	{
 		inputString = inputString.substring(0, inputString.length()-1);
 	}
@@ -57,8 +83,43 @@ public void keyPressed()
 		displayedMessages.add(inputString);
 		inputString = "";
 	}
-	else
+	else if (inputString.length() <= 70)
+	{
 		inputString += key;
+	}
+}
+class Quest
+{
+	protected int progress;
+	protected String initialMessage;
+    protected Cmd[] initialCommands;
+	public Quest(String initmsg, Cmd[] cmdlist)
+	{
+		progress = 0;
+		initialMessage = initmsg;
+		initialCommands = cmdlist;
+	}
+    public int getProgress()
+    {
+    	return progress;
+    }
+    public int incProgress()
+    {
+    	progress++;
+    	return progress;
+    }           
+}
+
+class Cmd
+{
+	String command;
+	String result;
+	ArrayList<Cmd> next;
+	public Cmd(String cmd, String rslt)
+	{
+		command = cmd;
+		result = rslt;
+	}
 }
   public void settings() { 	size(800, 500); }
   static public void main(String[] passedArgs) {
--- dontbesorry.pde
+++ dontbesorry.pde
@@ -1,12 +1,26 @@
-import java.util.Map;
+//import java.util.Map;
 ArrayList <String> displayedMessages = new ArrayList <String>();
 String inputString = "";
+StringDict[] quest1;
 public void setup()
 {
 	size(800, 500);
 	PFont mono;
 	mono = loadFont("Consolas-20.vlw");
 	textFont(mono);
+	Cmd[] initialCommands_quest1 = {
+		new Cmd(
+			"walk in",
+			"You enter the elder's home. He asks you for a reminder of your name."),
+		new Cmd(
+			"walk away",
+			"You walk away and return to your father's farm where you work the land for the rest of your life, and even have a couple of kids. You die at the age of 34 from tuberculosis with many regrets.")
+	};
+	Quest quest1 = new Quest(
+		"Welcome to Don't Be Sorry! You are a member of the plebian masses of Landia, your hometown. Yesterday you turned 16, the age of coming in your rather primal society, and chose the vocation of Adventurer. You are currently standing outside your respective elder's abode, contemplating entrance and seriously doubting yesterday's decision.",
+		initialCommands_quest1
+		);
+	displayedMessages.add(quest1.initialMessage);
 }
 public void draw()
 {
@@ -19,6 +33,19 @@ public void draw()
 	text(inputString, 15, height-15);
 	stroke(255);
 	line(0, height-40, width, height-40);
+	if (displayedMessages.size() != 0 && displayedMessages.get(displayedMessages.size()-1).length() >= 71)
+	{
+		String left_fragment = "";
+		String right_fragment = displayedMessages.get(displayedMessages.size()-1);
+		displayedMessages.remove(displayedMessages.size()-1);
+		while(right_fragment.length() >= 71)
+		{
+			left_fragment = right_fragment.substring(0, 70);
+			right_fragment = right_fragment.substring(70);
+			displayedMessages.add(left_fragment);
+		}
+		displayedMessages.add(right_fragment);
+	}
 	int j = displayedMessages.size() - 1;
 	for (int i = height-60; i > 60; i -= 30)
 	{
@@ -31,7 +58,7 @@ public void draw()
 }
 public void keyPressed()
 {
-	if (key == DELETE || key == BACKSPACE)
+	if ((key == DELETE || key == BACKSPACE) && inputString.length() != 0)
 	{
 		inputString = inputString.substring(0, inputString.length()-1);
 	}
@@ -40,15 +67,41 @@ public void keyPressed()
 		displayedMessages.add(inputString);
 		inputString = "";
 	}
-	else
+	else if (inputString.length() <= 70)
+	{
 		inputString += key;
+	}
 }
 class Quest
 {
 	protected int progress;
-	
-	public Quest
+	protected String initialMessage;
+    protected Cmd[] initialCommands;
+	public Quest(String initmsg, Cmd[] cmdlist)
 	{
 		progress = 0;
+		initialMessage = initmsg;
+		initialCommands = cmdlist;
+	}
+    public int getProgress()
+    {
+    	return progress;
+    }
+    public int incProgress()
+    {
+    	progress++;
+    	return progress;
+    }           
+}
+
+class Cmd
+{
+	String command;
+	String result;
+	ArrayList<Cmd> next;
+	public Cmd(String cmd, String rslt)
+	{
+		command = cmd;
+		result = rslt;
 	}
 }
