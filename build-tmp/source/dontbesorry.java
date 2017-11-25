c6799bd62c9b6e8cd1341d5f0bbca6f855614cf9
Sebastian Fischer <seabassfishes@gmail.com>
2017-11-25 12:25:21 -0800

    better wrapping, command replies work

--- build-tmp/source/dontbesorry.java
+++ build-tmp/source/dontbesorry.java
@@ -17,7 +17,8 @@ public class dontbesorry extends PApplet {
 //import java.util.Map;
 ArrayList <String> displayedMessages = new ArrayList <String>();
 String inputString = "";
-StringDict[] quest1;
+Quest quest1;
+Cmd[] currentCommands = new Cmd[10];
 public void setup()
 {
 	
@@ -32,20 +33,25 @@ public void setup()
 			"walk away",
 			"You walk away and return to your father's farm where you work the land for the rest of your life, and even have a couple of kids. You die at the age of 34 from tuberculosis with many regrets.")
 	};
-	Quest quest1 = new Quest(
+	quest1 = new Quest(
 		"Welcome to Don't Be Sorry! You are a member of the plebian masses of Landia, your hometown. Yesterday you turned 16, the age of coming in your rather primal society, and chose the vocation of Adventurer. You are currently standing outside your respective elder's abode, contemplating entrance and seriously doubting yesterday's decision.",
 		initialCommands_quest1
 		);
+	currentCommands = quest1.initialCommands;
+	for (Cmd cmd : currentCommands)
+	{
+		System.out.println(cmd.command);
+	}
 	displayedMessages.add(quest1.initialMessage);
 }
 public void draw()
 {
 	background(0);
 	fill(130);
-	rect(0, 0, width, 40);
+	rect(0, 0, width-1, 40);
 	fill(255);
 	textSize(20);
-	text("DON'T BE SORRY", 350, 25);
+	text("DON'T BE SORRY", width/2 -80, 25);
 	text(inputString, 15, height-15);
 	stroke(255);
 	line(0, height-40, width, height-40);
@@ -56,8 +62,17 @@ public void draw()
 		displayedMessages.remove(displayedMessages.size()-1);
 		while(right_fragment.length() >= 71)
 		{
-			left_fragment = right_fragment.substring(0, 70);
-			right_fragment = right_fragment.substring(70);
+			int x = 70;
+			while (right_fragment.charAt(x) != ' ')
+			{
+				x--;
+				if (x == -1) {
+					x = 70;
+					break;
+				}
+			}
+			left_fragment = right_fragment.substring(0, x);
+			right_fragment = right_fragment.substring(x+1);
 			displayedMessages.add(left_fragment);
 		}
 		displayedMessages.add(right_fragment);
@@ -81,6 +96,19 @@ public void keyPressed()
 	else if (key == ENTER || key == RETURN)
 	{
 		displayedMessages.add(inputString);
+		String result;
+		for (Cmd command : currentCommands)
+		{
+			System.out.println(command.command);
+			System.out.println(inputString);
+			if (inputString.equals(command.command))
+			{
+				System.out.println("Success on " + command.command);
+				displayedMessages.add(command.result);
+				//currentCommands = command.next.toArray(new Cmd[0]);
+				break;
+			}
+		}
 		inputString = "";
 	}
 	else if (inputString.length() <= 70)
@@ -90,16 +118,16 @@ public void keyPressed()
 }
 class Quest
 {
-	protected int progress;
-	protected String initialMessage;
-    protected Cmd[] initialCommands;
+	public int progress;
+	public String initialMessage;
+    public Cmd[] initialCommands;
 	public Quest(String initmsg, Cmd[] cmdlist)
 	{
 		progress = 0;
 		initialMessage = initmsg;
 		initialCommands = cmdlist;
 	}
-    public int getProgress()
+    /*public int getProgress()
     {
     	return progress;
     }
@@ -107,7 +135,7 @@ class Quest
     {
     	progress++;
     	return progress;
-    }           
+    }*/
 }
 
 class Cmd
